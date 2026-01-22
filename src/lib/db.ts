1de22ea263a7c5fbfb7e4733a15733e32d5c8037
Claudenir Nojosa <clau.nojosaf@gmail.com>
2026-01-22 13:12:27 -0300

    fluxo cancela quando username nao é encontrado - fixed

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from '@prisma/client';
 declare global {
   var prisma: PrismaClient | undefined;
 }
-
+// teste
 const db = globalThis.prisma || new PrismaClient({
   log: process.env.NODE_ENV === 'development' 
     ? ['query', 'error', 'warn'] 
